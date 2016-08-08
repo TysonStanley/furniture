ba04dc21b48d971c557728b199f0c2fe20765ae7
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-08-08 13:53:42 -0600

    Update mirror.R

--- R/mirror.R
+++ R/mirror.R
@@ -1,10 +1,10 @@
 
 mirror = function(data, variable, lower, upper, replace=FALSE){
-  .rc = data.frame("Original"=NA, "Recode"=NA)
+  .rc = data.frame(".Original"=NA, ".Recode"=NA)
   for(i in 0:(upper-lower)){
     .rc[i+1, 1:2] = c(i+1, upper - i)
   }
-  data = merge(data, .rc, by.x=variable, by.y="Original")
+  data = merge(data, .rc, by.x=variable, by.y=".Original")
   if (replace){
     data[, which(names(data) == variable)] = data[dim(data)[2]]
     data[dim(data)[2]] = NULL
