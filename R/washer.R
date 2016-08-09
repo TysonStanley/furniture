e2ee73914c6ac5f9cfb7c36825c51ee8ac4c2b26
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-08-09 13:34:36 -0600

    Update washer.R
    Adding the ability to use functions to change values (e.g., is.na, is.nan)

--- R/washer.R
+++ R/washer.R
@@ -1,7 +1,15 @@
 
 washer = function(x, ..., value=NA){
-  for (i in c(...)){
-    x[x == i] = value
+  if (is.factor(x)){
+    x = as.character(x)
+  }
+  if (is.function(list(...)[[1]])){
+    .f = function(x) list(...)[[1]]
+    x[list(...)[[1]](x)] = value
+  } else {
+    for (i in c(...)){
+      x[x == i] = value
+    }
   }
   return(x)
 }
