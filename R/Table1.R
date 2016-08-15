9505f725eefce3084529d65ba6588f82f0f105d7
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-08-14 23:42:59 -0600

    Print Method for Table1

--- R/Table1.R
+++ R/Table1.R
@@ -289,9 +289,7 @@ table1 = function(data, vars, splitby=NULL, splitby_labels = NULL,
 }
 
 print.table1_df <- function(x, ...){
-  cat("---\n")
   print(x, row.names = FALSE, ...)
-  cat("---\n")
 }
 
 
