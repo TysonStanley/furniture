f793a59ab626fab87ca1544df7c84f9e096c5625
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-10-07 23:47:50 -0600

    Updated Depends and Imports

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname = find.package("furniture"), pkgname = "furniture") {
+  packageStartupMessage("furniture 1.1.0: learn more at tysonstanley.github.io")
+}
--- tests/testthat/test_table1.R
+++ tests/testthat/test_table1.R
@@ -12,5 +12,6 @@ test_that("table1 produces table1", {
   
   expect_s3_class(table1(df, x, y, z, factor(a)), "table1")
   expect_error(table1(df, b))
+  expect_s3_class(table1(df, a, x, y, splitby=~factor(z)), "table1")
 })
 
