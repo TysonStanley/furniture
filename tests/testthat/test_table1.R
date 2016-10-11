bcef4428eb852bc75da315fd9890719ffd7393cb
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-10-11 17:14:27 -0600

    Updating table1 tests

--- tests/testthat/test_table1.R
+++ tests/testthat/test_table1.R
@@ -11,53 +11,63 @@ test_that("table1 produces table1", {
   x <- rep(0, times=7)
   b <- rep(0, times=7)
   
+  ## Simple
   expect_s3_class(table1(df, x, y, z, factor(a)), "table1")
-  
+  ## b not in df
   expect_error(table1(df, b))
-  
+  ## Testing splitby
   expect_s3_class(table1(df, a, x, y, splitby=~factor(z)), "table1")
-  
+  ## Testing test
   expect_s3_class(table1(df, a, x, y, splitby=~factor(z), 
                          test=TRUE), "table1")
-  
+  ## Other output types
   expect_s3_class(table1(df, a, x, y, 
                          splitby=~factor(z), 
                          test=TRUE, 
                          output_type = "markdown", 
                          align = c("l", "c", "c")), "knitr_kable")
-  
+  ## Alternative splitby
   expect_s3_class(table1(df, z, x, y, splitby="a", 
                          test=TRUE), "table1")
-  
+  ## OTher formats
   expect_s3_class(table1(df, z, x, y, splitby=~a, 
                          test=TRUE, 
                          format_output = "full"), "table1")
-  
   expect_s3_class(table1(df, z, x, y, splitby=~a, 
                          test=TRUE, 
                          format_output = "stars"), "table1")
-  
+  ## Error of quotes
   expect_error(table1(df, "a", splitby=~z))
-  
+  ## Different splitby produce same
   expect_equivalent(table1(df, a, x, y, splitby=~z), table1(df, a, x, y, splitby="z"))
-  
+  ## Variable names
   expect_s3_class(table1(df, z, x, y, splitby=~a, 
                          test=TRUE, 
                          var_names = c("Z", "X", "Y")), "table1")
-  
+  ## splitby labels
   expect_s3_class(table1(df, z, x, y, splitby=~a, 
                          test=TRUE, 
                          splitby_labels = c("male", "female")), "table1")
-  
+  ## NAkeep
   expect_s3_class(table1(df, z, x, y, splitby=~a, 
                          test=TRUE, 
                          NAkeep = TRUE), "table1")
-  
+  ## Piping
   expect_equal(table1(df, a, x, y, splitby=~z, piping=TRUE), df)
-  
   expect_equivalent(table1(df, a, x, splitby=~z, 
                            piping = TRUE), 
                     table1(df, a, x, y, splitby="z", output_type = "markdown",
                            piping = TRUE))
+  ## Test type == or
+  expect_s3_class(table1(df, z, x, y, splitby=~a, 
+                         test=TRUE, test_type = "or",
+                         NAkeep = TRUE), "table1")
+  ## Taking a character var as the splitby
+  expect_s3_class(table1(df, z, x, y, splitby=~as.character(a), 
+                         test=TRUE), "table1")
+  ## Index
+  expect_s3_class(table1(df, c(1:3), splitby=~a, 
+                         test=TRUE, 
+                         NAkeep = TRUE), "table1")
 })
 
