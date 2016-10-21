18633a644e26c7538235d4c4b38f23a803d9a592
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-10-20 22:55:42 -0600

    furniture package doc
    added additional information on table 1

--- R/furniture.R
+++ R/furniture.R
@@ -1,10 +1,10 @@
-
 #' furniture
 #' 
-#' The furniture package offers simple functions and an operator that are aimed at helping
-#' applied researchers explore and communicate their data as well as clean their data
-#' in a tidy way. The package follows similar semantics to the "tidyverse" packages. It contains 
-#' two main tools (along with two operators):
+#' The furniture package offers simple functions (i.e. pieces of furniture) and an 
+#' operator that are aimed at helping applied researchers explore and communicate 
+#' their data as well as clean their data in a tidy way. The package follows similar 
+#' semantics to the "tidyverse" packages. It contains two main tools (along with an 
+#' operator):
 #' \itemize{
 #'   \item \code{table1} provides a well-formatted descriptive table often seen 
 #'         as table 1 in academic journals,
@@ -14,12 +14,14 @@
 #'         tests for significance via a chi-square test.
 #' }
 #' 
-#' It works similar to operators in packages like \code{magrittr}.
-#' 
-#' Table 1 is the main function in furniture. It is important in both data exploration 
+#' Table 1 is the main function in furniture. It is useful in both data exploration 
 #' and data communication. With minimal cleaning, the outputted table can be put into 
-#' an academic, peer reviewed journal manuscript.
-#' 
+#' an academic, peer reviewed journal manuscript. As such, it is very useful in exploring
+#' your data when you have a stratifying variable. For example, if you are exploring whether
+#' the means of several demographic and behavioral characteristics are related to a health condition,
+#' the health condition (i.e. "yes" or "no"; "low", "mid", or "high"; or a list of conditions) as
+#' the stratifying variable. With little code, you can test for associations and check means or 
+#' counts by the stratifying variable. See the vignette for more information. 
 #' 
 #' 
 #' @examples
