ecb0d12d2cf46c149883cc739fa393e31d3d4c6a
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2016-11-29 16:18:41 -0700

    update washer test file

--- tests/testthat/test_washer.R
+++ tests/testthat/test_washer.R
@@ -4,5 +4,6 @@ library(testthat)
 test_that("washer produces correct output vectors", {
   expect_equal(washer(c(1,2,3,4), 1), c(NA,2,3,4))
   expect_equal(washer(c(1,2,3,4), 1, value=0), c(0,2,3,4))
+  expect_equal(washer(as.character(c(1,2,3,4)), 1, value=0), as.character(c(0,2,3,4)))
 })
 
