06ed232e2cf9b94f812d1b65fda49895a701817c
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2017-09-16 10:55:35 -0600

    updated website

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname = find.package("furniture"), pkgname = "furniture") {
-  packageStartupMessage("furniture 1.6.0: learn more at tysonstanley.github.io")
+  packageStartupMessage("furniture 1.6.0: learn more at tysonbarrett.com")
 }
