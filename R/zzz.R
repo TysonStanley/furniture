732929b50253d14c155c7aa7d9f931fda174f77c
Tyson S. Barrett <t.barrett88@gmail.com>
2018-05-17 18:58:02 -0600

    added dplyr in suggests for unit tests

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname = find.package("furniture"), pkgname = "furniture") {
-  packageStartupMessage("furniture 1.7.8: learn more at tysonbarrett.com")
+  packageStartupMessage("furniture 1.7.9: learn more at tysonbarrett.com")
 }
