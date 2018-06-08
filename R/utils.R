e88de4d745d479fffca68e2dfc9f8a6b42e5b50d
Tyson S. Barrett <t.barrett88@gmail.com>
2018-06-08 17:03:04 -0600

    group_by and onAttach
    re export group_by and added several features to when the package is attached

--- R/utils.R
+++ R/utils.R
@@ -126,6 +126,52 @@
   return(num_fun2)
 }
 
+## From tidyverse package
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+  
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+  
+  theme <- rstudioapi::getThemeInfo()
+  
+  if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+  
+}
+
+furniture_version <- function(x) {
+  version <- as.character(unclass(utils::packageVersion(x))[[1]])
+  crayon::italic(paste0(version, collapse = "."))
+}
+
+search_conflicts <- function(path = search()){
+  
+  ## Search for conflicts
+  confs <- conflicts(path,TRUE)
+  ## Grab those with the furniture package
+  furniture_conflicts <- confs$`package:furniture`
+  
+  ## Find which packages have those functions that are conflicted
+  if (length(furniture_conflicts) != 0){
+    other_conflicts <- list()
+    for (i in furniture_conflicts){
+      other_conflicts[[i]] <- lapply(confs, function(x) any(grepl(i, x))) %>%
+        do.call("rbind", .) %>%
+        data.frame %>%
+        setNames(c("conflicted")) %>%
+        tibble::rownames_to_column() %>%
+        .[.$conflicted == TRUE & .$rowname != "package:furniture",]
+    }
+  } else {
+    other_conflicts <- list()
+  }
+  other_conflicts
+}
+
 #' re-export magrittr pipe operator
 #'
 #' @importFrom magrittr %>%
@@ -134,3 +180,9 @@
 #' @export
 NULL
 
+#' re-export dplyr group_by function
+#'
+#' @importFrom dplyr group_by
+#' @name group_by
+#' @export
+NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,26 @@
 .onAttach <- function(libname = find.package("furniture"), pkgname = "furniture") {
-  packageStartupMessage("furniture 1.7.10: learn more at tysonbarrett.com")
+  
+  confs <- search_conflicts() %>%
+    do.call("rbind", .)
+  if (length(confs) == 0){
+    confs_msg <- text_col(paste0(crayon::green(cli::symbol$tick), " No function conflicts found."))
+  } else {
+    confs_msg <- text_col(paste0(crayon::yellow(cli::symbol$cross),
+                                 " The furniture::", rownames(confs), "() function is conflicted with ", 
+                                 gsub("package:", "", confs$rowname),
+                                 "::", rownames(confs), "()", "\n",
+                                 crayon::italic("   Consider using `furniture::` for each function call.")))
+  }
+  
+  packageStartupMessage(text_col(cli::cat_rule(left = paste0("furniture ", furniture_version("furniture")), 
+                                               right = "learn more at tysonbarrett.com")),
+                        confs_msg)
+  
+}
+
+.onLoad <- function(libname = find.package("furniture"), pkgname = "furniture"){
+  if(getRversion() >= "2.15.1") {
+    utils::globalVariables(".")
+  }
+  invisible()
 }
