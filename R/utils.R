99f608dd0878fc558720c0b19a0df24f180245f4
TysonStanley <t.barrett@aggiemail.usu.edu>
2018-06-09 17:00:29 -0600

    startup message

--- R/utils.R
+++ R/utils.R
@@ -166,7 +166,7 @@ search_conflicts <- function(path = search()){
         tibble::rownames_to_column() %>%
         .[.$conflicted == TRUE & 
             .$rowname != "package:furniture" &
-            .$rowname != "%>%",]
+            grepl("%\\>%", .$rowname),]
     }
   } else {
     other_conflicts <- list()
--- R/zzz.R
+++ R/zzz.R
@@ -4,17 +4,19 @@
     do.call("rbind", .)
   if (length(confs) == 0){
     confs_msg <- text_col(paste0(crayon::green(cli::symbol$tick), " No conflicts found."))
+    helper_msg <- ""
   } else {
     confs_msg <- text_col(paste0(crayon::yellow(cli::symbol$cross),
                                  " The furniture::", rownames(confs), "() function is conflicted with ", 
                                  gsub("package:", "", confs$rowname),
-                                 "::", rownames(confs), "", "\n",
-                                 crayon::italic("   Consider using `furniture::` for each function call.\n")))
+                                 "::", rownames(confs), "", "\n"))
+    helper_msg <- crayon::italic("   Consider using `furniture::` for each function call.\n")
   }
   
   packageStartupMessage(text_col(cli::cat_rule(left = paste0("furniture ", furniture_version("furniture")), 
                                                right = "learn more at tysonbarrett.com")),
-                        confs_msg)
+                        confs_msg,
+                        helper_msg)
   
 }
 
